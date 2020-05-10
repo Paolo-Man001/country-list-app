da8458ced0b729254b02f8a502b9946caec73154
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-10 23:11:28 +1200

    ft-1-2-impl-ViewModel : branch-commit-v1   Setup ListViewModel as subclass of ViewModel-lifecycle.   Initialize MutableLiveData Objects. Declare entry-point-method(refresh()) that fetches a List-Of-Countries.

--- app/src/main/java/com/paolomanlunas/countriesapp/viewmodel/ListViewModel.java
+++ app/src/main/java/com/paolomanlunas/countriesapp/viewmodel/ListViewModel.java
@@ -1,4 +1,29 @@
 package com.paolomanlunas.countriesapp.viewmodel;
 
-public class ListViewModel {
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.paolomanlunas.countriesapp.model.CountryModel;
+
+import java.util.List;
+
+public class ListViewModel extends ViewModel {
+
+   /** LiveData is an Object that generates Values.
+    *    These values are generated asynchoronously.
+    *    They are OBSERVABLES which values can be observed.
+    *
+   * */
+   public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<>();
+   public MutableLiveData<Boolean> countryLoadError = new MutableLiveData<>();
+   public MutableLiveData<Boolean> loading = new MutableLiveData<>();
+
+
+   public void refresh() {
+      fetchCountries();
+   }
+
+   private void fetchCountries() {
+
+   }
 }
