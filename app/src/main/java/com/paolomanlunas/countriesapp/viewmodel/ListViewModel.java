0f79f281e2564ca2914b4174278eec0cd3d07692
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-10 23:27:10 +1200

    ft-1-2-impl-ViewModel : branch-commit-v2(fin)   ViewModel - Create Mockdat List of Countries in fetchCountries().

--- app/src/main/java/com/paolomanlunas/countriesapp/viewmodel/ListViewModel.java
+++ app/src/main/java/com/paolomanlunas/countriesapp/viewmodel/ListViewModel.java
@@ -5,15 +5,17 @@ import androidx.lifecycle.ViewModel;
 
 import com.paolomanlunas.countriesapp.model.CountryModel;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListViewModel extends ViewModel {
 
-   /** LiveData is an Object that generates Values.
-    *    These values are generated asynchoronously.
-    *    They are OBSERVABLES which values can be observed.
-    *
-   * */
+   /**
+    * LiveData is an Object that generates Values.
+    * These values are generated asynchoronously.
+    * They are OBSERVABLES which values can be observed.
+    */
    public MutableLiveData<List<CountryModel>> countries = new MutableLiveData<>();
    public MutableLiveData<Boolean> countryLoadError = new MutableLiveData<>();
    public MutableLiveData<Boolean> loading = new MutableLiveData<>();
@@ -24,6 +26,18 @@ public class ListViewModel extends ViewModel {
    }
 
    private void fetchCountries() {
+      /*
+       * Temporary Data(hard-coded) List of Counties
+       * */
 
+      List<CountryModel> list = new ArrayList<>();
+      list.add(new CountryModel("Albania", "Tirana", ""));
+      list.add(new CountryModel("Brazil", "Brasilia", ""));
+      list.add(new CountryModel("Czech Republic", "Prague", ""));
+      list.add(new CountryModel("Denmark", "Copenhagen", ""));
+
+      countries.setValue(list);
+      countryLoadError.setValue(false);
+      loading.setValue(false);            // progress-bar-Spinner
    }
 }
