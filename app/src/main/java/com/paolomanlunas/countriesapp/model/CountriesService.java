7dcd929e1158d0ec0cfd2150ad7ca5e0aa175940
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-12 15:47:11 +1200

    ft-2-1-impl-retorfit-rxjava :   In 'model' package, Create CountriesApi(Interface) and CountriesService(Implementation).   Create InterfaceMethod - getCountries() @GET annotated from retorfit. return List-of-'Single'(an rxjava component observable-type object).   Create class-Singleton for CountriesService. Implement retorfit.builder() to convert backend JSON into CountryModel as a List-of-SingletonType.   Annotate CountryModel with @SerializedName to match the JSON 'key' and save value in Annotated Model-Property.

---
+++ app/src/main/java/com/paolomanlunas/countriesapp/model/CountriesService.java
@@ -0,0 +1,38 @@
+package com.paolomanlunas.countriesapp.model;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class CountriesService {
+   private static final String BASE_URL = "https://raw.githubusercontent.com";
+
+   private static CountriesService instance;
+
+   private CountriesApi api = new Retrofit.Builder()
+           .baseUrl(BASE_URL)
+           .addConverterFactory(GsonConverterFactory.create())          // Converts JSON-res into Obj. of Country-MODEL we defined, as a LIST-of-CountryModel.
+           .addCallAdapterFactory(RxJava2CallAdapterFactory.create())   // Converts the List<CountryModel> into an RxJava Component(Single) Observable
+           .build()
+           .create(CountriesApi.class);
+
+   //--- Constructor:
+   private CountriesService() {
+   }
+   // Make this class as a SINGLETON:
+   public static CountriesService getInstance() {
+      if (instance == null) {
+         instance = new CountriesService();
+      }
+      return instance;
+   }
+
+
+   //-- getCountries LIST created by Builder()
+   public Single<List<CountryModel>> getCountries() {
+      return api.getCountries();
+   }
+}
--- app/src/main/java/com/paolomanlunas/countriesapp/model/CountryModel.java
+++ app/src/main/java/com/paolomanlunas/countriesapp/model/CountryModel.java
@@ -1,8 +1,16 @@
 package com.paolomanlunas.countriesapp.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class CountryModel {
+
+   @SerializedName("name")    // @SerializedName is used to match the 'Key' from JSON data
    String countryName;
+
+   @SerializedName("capital")
    String capital;
+
+   @SerializedName("flagPNG")
    String flag;
 
    public CountryModel(String countryName, String capital, String flag) {
