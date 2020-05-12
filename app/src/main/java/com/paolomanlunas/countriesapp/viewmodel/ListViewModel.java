dccc740dd912000ca8aaaebe5e9d0efc568ea1a7
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-12 16:39:26 +1200

    ft-2-2-get-backend-data:   Enable Internet-Communication in AndroidManifest.xml file.   Setup Fetching JSON response DATA from backend ListViewModel.java.   Use CompositeDisposable class to Enable fetching JSON in the background-process while observing/listening in the MainThread.

--- app/src/main/java/com/paolomanlunas/countriesapp/viewmodel/ListViewModel.java
+++ app/src/main/java/com/paolomanlunas/countriesapp/viewmodel/ListViewModel.java
@@ -3,11 +3,17 @@ package com.paolomanlunas.countriesapp.viewmodel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.paolomanlunas.countriesapp.model.CountriesService;
 import com.paolomanlunas.countriesapp.model.CountryModel;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
 
 public class ListViewModel extends ViewModel {
 
@@ -20,32 +26,67 @@ public class ListViewModel extends ViewModel {
    public MutableLiveData<Boolean> countryLoadError = new MutableLiveData<>();
    public MutableLiveData<Boolean> loading = new MutableLiveData<>();
 
+   private CountriesService countriesService = CountriesService.getInstance();
+      // 'CompositeDisposable' allows us to intercept the countriesService to dispose its background-process(fetch JSON).
+      // Dispose process when finished fetching back-end data prevents memory-loss.
+   private CompositeDisposable disposable = new CompositeDisposable();
 
    public void refresh() {
       fetchCountries();
    }
 
+   // GET Data from backend
    private void fetchCountries() {
-      /*
-       * Temporary Data(hard-coded) List of Counties
-       * */
+      loading.setValue(true);       // ProgressBar START
+      disposable.add(
+              countriesService.getCountries()
+                      .subscribeOn(Schedulers.newThread()) // puts this process on a New Thread(background-process while waiting for API response)
+                      .observeOn(AndroidSchedulers.mainThread())  // When the RESPONSE is available, put it on the Main-Thread-process, to UPDATE our view.
+                      .subscribeWith(new DisposableSingleObserver<List<CountryModel>>() { // Subscribe to RxJava Observable-Component 'Single'
 
-      List<CountryModel> list = new ArrayList<>();
-      list.add(new CountryModel("Albania", "Tirana", ""));
-      list.add(new CountryModel("Brazil", "Brasilia", ""));
-      list.add(new CountryModel("Czech Republic", "Prague", ""));
-      list.add(new CountryModel("Denmark", "Copenhagen", ""));
-      list.add(new CountryModel("Albania", "Tirana", ""));
-      list.add(new CountryModel("Brazil", "Brasilia", ""));
-      list.add(new CountryModel("Czech Republic", "Prague", ""));
-      list.add(new CountryModel("Denmark", "Copenhagen", ""));
-      list.add(new CountryModel("Albania", "Tirana", ""));
-      list.add(new CountryModel("Brazil", "Brasilia", ""));
-      list.add(new CountryModel("Czech Republic", "Prague", ""));
-      list.add(new CountryModel("Denmark", "Copenhagen", ""));
+                         @Override
+                         public void onSuccess(List<CountryModel> countryModels) {
+                            countries.setValue(countryModels); // GET List-of-Countries
+                            countryLoadError.setValue(false);  // NoError
+                            loading.setValue(false);           // ProgressBar STOP
+                         }
 
-      countries.setValue(list);
-      countryLoadError.setValue(false);
-      loading.setValue(false);            // progress-bar-Spinner
+                         @Override
+                         public void onError(Throwable e) {
+                            countryLoadError.setValue(true);   // NoError
+                            loading.setValue(false);           // ProgressBar STOP
+                            e.printStackTrace();
+                         }
+                      })
+      ); // END: disposable.add()
    }
+
+   @Override
+   protected void onCleared() {
+      super.onCleared();
+      disposable.clear();
+   }
+
+   /* ****
+    * Temporary Local-Data(hard-coded) List of Counties
+    * *** */
+//   private void fetchCountries() {
+//      List<CountryModel> list = new ArrayList<>();
+//      list.add(new CountryModel("Albania", "Tirana", ""));
+//      list.add(new CountryModel("Brazil", "Brasilia", ""));
+//      list.add(new CountryModel("Czech Republic", "Prague", ""));
+//      list.add(new CountryModel("Denmark", "Copenhagen", ""));
+//      list.add(new CountryModel("Albania", "Tirana", ""));
+//      list.add(new CountryModel("Brazil", "Brasilia", ""));
+//      list.add(new CountryModel("Czech Republic", "Prague", ""));
+//      list.add(new CountryModel("Denmark", "Copenhagen", ""));
+//      list.add(new CountryModel("Albania", "Tirana", ""));
+//      list.add(new CountryModel("Brazil", "Brasilia", ""));
+//      list.add(new CountryModel("Czech Republic", "Prague", ""));
+//      list.add(new CountryModel("Denmark", "Copenhagen", ""));
+//
+//      countries.setValue(list);
+//      countryLoadError.setValue(false);
+//      loading.setValue(false);            // progress-bar-Spinner
+//   }
 }
